b6aedc9bf04d253ce8e240b5e8398940570d5606
jarsulk <jsjsjs000@gmail.com>
2023-05-19 15:42:18 +0200

    dev

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	printf("i.MX controller\r\n");
+	return 0;
+}
+
+/*
+
+	Compile and run:
+make
+	or in Visual Studio Code with extension "Makefile Tools":
+Ctrl+Shift+B
+
+*/
